5aeb3878b4fee33530301a0ed438f70f46bd8fcc
sewon-31 <oneoctober12@gmail.com>
2025-07-29 16:13:20 +0900

    [Refactor] refactor unit tests

--- SSD/command_parser_test.cpp
+++ SSD/command_parser_test.cpp
@@ -12,6 +12,7 @@ public:
 
 	const string CMD_READ = "R";
 	const string CMD_WRITE = "W";
+	const string CMD_INVALID = "D";
 
 	const string VALID_LBA = "3";
 	const string INVALID_LBA_OUT_OF_RANGE = "100";
@@ -20,66 +21,67 @@ public:
 	const string VALID_VALUE = "0xAAAABBBB";
 	const string INVALID_VALUE1 = "0xG112BBBB";
 	const string INVALID_VALUE2 = "0xAAA";
+	const string INVALID_VALUE3 = "FFFFFFFFFF";
+
+	string createCommand(const vector<string>& strs) {
+		std::ostringstream oss;
+		for (size_t i = 0; i < strs.size(); ++i) {
+			oss << strs[i];
+			if (i != strs.size() - 1)
+				oss << " ";
+		}
+
+		return oss.str();
+	}
 };
 
 TEST_F(SSDCommandParserTestFixture, InvalidAppName)
 {
-	string cmdStr = INVALID_APP_NAME + " " + CMD_READ + " " + VALID_VALUE;
-	parser.setCommand(cmdStr);
+	parser.setCommand(createCommand({ INVALID_APP_NAME, CMD_READ, VALID_VALUE }));
 	EXPECT_FALSE(parser.validateCommand());
 }
 
 TEST_F(SSDCommandParserTestFixture, InvalidParameterCount1)
 {
-	string cmdStr = APP_NAME;
-	parser.setCommand(cmdStr);
+	parser.setCommand(createCommand({ APP_NAME }));
 	EXPECT_FALSE(parser.validateCommand());
 
-	cmdStr = APP_NAME + " " + CMD_READ + " " + VALID_LBA + " " + VALID_VALUE + " " + VALID_VALUE;
-	parser.setCommand(cmdStr);
+	parser.setCommand(createCommand({ APP_NAME, CMD_READ, VALID_LBA, VALID_VALUE, VALID_VALUE }));
 	EXPECT_FALSE(parser.validateCommand());
 }
 
 TEST_F(SSDCommandParserTestFixture, InvalidOperationCommand)
 {
-	string cmdStr = APP_NAME + " E " + VALID_VALUE;
-	parser.setCommand(cmdStr);
+	parser.setCommand(createCommand({ APP_NAME, CMD_INVALID, VALID_VALUE }));
 	EXPECT_FALSE(parser.validateCommand());
 }
 
 TEST_F(SSDCommandParserTestFixture, InvalidParameterCount2)
 {
-	string cmdStr = APP_NAME + " " + CMD_WRITE + " " + VALID_LBA;
-	parser.setCommand(cmdStr);
+	parser.setCommand(createCommand({ APP_NAME, CMD_WRITE, VALID_LBA }));
 	EXPECT_FALSE(parser.validateCommand());
 
-	cmdStr = APP_NAME + " " + CMD_READ + " " + VALID_LBA + " " + VALID_VALUE;
-	parser.setCommand(cmdStr);
+	parser.setCommand(createCommand({ APP_NAME, CMD_READ, VALID_LBA, VALID_VALUE }));
 	EXPECT_FALSE(parser.validateCommand());
 }
 
 TEST_F(SSDCommandParserTestFixture, InvalidLBA)
 {
-	string cmdStr = APP_NAME + " " + CMD_READ + " " + INVALID_LBA_OUT_OF_RANGE;
-	parser.setCommand(cmdStr);
+	parser.setCommand(createCommand({ APP_NAME, CMD_READ, INVALID_LBA_NOT_A_NUMBER }));
 	EXPECT_FALSE(parser.validateCommand());
 
-	cmdStr = APP_NAME + " " + CMD_READ + " " + INVALID_LBA_NOT_A_NUMBER;
-	parser.setCommand(cmdStr);
+	parser.setCommand(createCommand({ APP_NAME, CMD_READ, INVALID_LBA_OUT_OF_RANGE }));
 	EXPECT_FALSE(parser.validateCommand());
 }
 
 TEST_F(SSDCommandParserTestFixture, InvalidValue)
 {
-	string cmdStr = APP_NAME + " " + CMD_WRITE + " " + VALID_LBA + " " + INVALID_VALUE1;
-	parser.setCommand(cmdStr);
+	parser.setCommand(createCommand({ APP_NAME, CMD_WRITE, VALID_LBA, INVALID_VALUE1 }));
 	EXPECT_FALSE(parser.validateCommand());
 
-	cmdStr = APP_NAME + " " + CMD_WRITE + " " + VALID_LBA + " " + INVALID_VALUE2;
-	parser.setCommand(cmdStr);
+	parser.setCommand(createCommand({ APP_NAME, CMD_WRITE, VALID_LBA, INVALID_VALUE2 }));
 	EXPECT_FALSE(parser.validateCommand());
 
-	cmdStr = APP_NAME + " " + CMD_WRITE + " " + VALID_LBA + " FFFFFFFFFF";
-	parser.setCommand(cmdStr);
+	parser.setCommand(createCommand({ APP_NAME, CMD_WRITE, VALID_LBA, INVALID_VALUE3 }));
 	EXPECT_FALSE(parser.validateCommand());
 }
