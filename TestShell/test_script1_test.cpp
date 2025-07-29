ad4309c5e20fae511210fe4c3f3cae55c0b603fe
naing.yoon <naing.yoon@samsung.com>
2025-07-30 08:12:45 +0900

    [Refactoring] Move common parameter to TestFixture

--- TestShell/test_script1_test.cpp
+++ TestShell/test_script1_test.cpp
@@ -7,46 +7,45 @@ using namespace std;
 
 class TestScript1Fixture : public Test {
 public:
+	void SetUp() override {
+		script = new FullWriteAndReadCompare(mockSSD);
+	}
+	NiceMock<MockSSD> mockSSD;
+	string invalidData = "0xFFFFFFFF";
+	FullWriteAndReadCompare* script;
+	ostringstream oss;
 };
 
 TEST_F(TestScript1Fixture, Success) {
-	NiceMock<MockSSD> mockSSD;
-	FullWriteAndReadCompare script(mockSSD);
-	ostringstream oss;
 	auto oldCoutStreamBuf = std::cout.rdbuf();
 	std::cout.rdbuf(oss.rdbuf());
-	int lba = 0;
-
+	
 	EXPECT_CALL(mockSSD, write(_, _))
 		.Times(100);
 
 	for (int lba = 0; lba < 100; lba++) {
-		string expectData = script.getExpectData(lba);
+		string expectData = script->getExpectData(lba);
 		EXPECT_CALL(mockSSD, read(lba))
 			.Times(1)
 			.WillOnce(Return(expectData));
 	}
 
-	script.Run();
+	script->Run();
 
 	std::cout.rdbuf(oldCoutStreamBuf);
 	EXPECT_EQ("PASS", oss.str());
 }
 
 TEST_F(TestScript1Fixture, FailWithInvalidData) {
-	NiceMock<MockSSD> mockSSD;
-	FullWriteAndReadCompare script(mockSSD);
-	string invalidData = "0xFFFFFFFF";
-	ostringstream oss;
 	auto oldCoutStreamBuf = std::cout.rdbuf();
 	std::cout.rdbuf(oss.rdbuf());
 
 	EXPECT_CALL(mockSSD, read(0))
-		.WillOnce(Return(script.getExpectData(0)));
+		.WillOnce(Return(script->getExpectData(0)));
 	EXPECT_CALL(mockSSD, read(1))
 		.WillOnce(Return(invalidData));
 
-	script.Run();
+	script->Run();
 
 	std::cout.rdbuf(oldCoutStreamBuf);
 	EXPECT_EQ("FAIL", oss.str());
