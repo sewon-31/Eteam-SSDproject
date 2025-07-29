7ac4dcb02007d816d31246b758b0e14b2b45d4d5
ies.cho <ies.cho@sec.com>
2025-07-29 16:25:20 +0900

    [refactor] Added multiline write/read TC

--- SSD/FileName_Test.cpp
+++ SSD/FileName_Test.cpp
@@ -5,55 +5,43 @@
 
 using namespace testing;
 
-class TCFixture :public testing::Test {
+class FileInterfaceFixture :public testing::Test {
 public:
-
-};
-
-TEST(FILEINTERFACE, TC_BASE) {
-
-	EXPECT_EQ(1,1);
-}
-
-TEST(FILEINTERFACE, TC_FILE_OPEN) {
-	FileInterface FI;
-	EXPECT_TRUE(FI.fileOpen("ssd_nand.txt"));
-	EXPECT_TRUE(FI.fileClose());
-}
-
-TEST(FILEINTERFACE, TC_FILE_OPEN_WRITE) {
-	FileInterface FI;
-	std::string expected_str = "12341234\n";
-	std::string read_str;
-
-	EXPECT_TRUE(FI.fileOpen("ssd_nand.txt"));
-	EXPECT_TRUE(FI.fileWrite(expected_str));
-	EXPECT_TRUE(FI.fileRead(read_str));
-	EXPECT_EQ(read_str+"\n", expected_str);
-	EXPECT_TRUE(FI.fileClose());
-}
-
-TEST(FILEINTERFACE, TC_FILE_OPEN_WRITE_MultiLine) {
-	FileInterface FI;
+	FileInterface fileInterface;
 	std::string expected_str = "12341234\n";
 	std::string expected_str1 = "22341234\n";
 	std::string expected_str2 = "32341234\n";
 	std::string expected_str3 = "42341234\n";
 	std::string read_str;
+};
 
-	EXPECT_TRUE(FI.fileOpen("ssd_nand.txt"));
-	EXPECT_TRUE(FI.fileWrite(expected_str));
-	EXPECT_TRUE(FI.fileWrite(expected_str1));
-	EXPECT_TRUE(FI.fileWrite(expected_str2));
-	EXPECT_TRUE(FI.fileWrite(expected_str3));
-	EXPECT_TRUE(FI.fileRead(read_str));
+TEST_F(FileInterfaceFixture, TC_FILE_OPEN) {
+	EXPECT_TRUE(fileInterface.fileOpen("ssd_nand.txt"));
+	EXPECT_TRUE(fileInterface.fileClose());
+}
+
+TEST_F(FileInterfaceFixture, TC_FILE_OPEN_WRITE) {
+	EXPECT_TRUE(fileInterface.fileOpen("ssd_nand.txt"));
+	EXPECT_TRUE(fileInterface.fileWrite(expected_str));
+	EXPECT_TRUE(fileInterface.fileRead(read_str));
+	EXPECT_EQ(read_str+"\n", expected_str);
+	EXPECT_TRUE(fileInterface.fileClose());
+}
+
+TEST_F(FileInterfaceFixture, TC_FILE_OPEN_WRITE_MultiLine) {
+	EXPECT_TRUE(fileInterface.fileOpen("ssd_nand.txt"));
+	EXPECT_TRUE(fileInterface.fileWrite(expected_str));
+	EXPECT_TRUE(fileInterface.fileWrite(expected_str1));
+	EXPECT_TRUE(fileInterface.fileWrite(expected_str2));
+	EXPECT_TRUE(fileInterface.fileWrite(expected_str3));
+	EXPECT_TRUE(fileInterface.fileRead(read_str));
 	EXPECT_EQ(read_str + "\n", expected_str);
-	EXPECT_TRUE(FI.fileRead(read_str));
+	EXPECT_TRUE(fileInterface.fileRead(read_str));
 	EXPECT_EQ(read_str + "\n", expected_str);
-	EXPECT_TRUE(FI.fileRead(read_str));
+	EXPECT_TRUE(fileInterface.fileRead(read_str));
 	EXPECT_EQ(read_str + "\n", expected_str);
-	EXPECT_TRUE(FI.fileRead(read_str));
+	EXPECT_TRUE(fileInterface.fileRead(read_str));
 	EXPECT_EQ(read_str + "\n", expected_str);
 
-	EXPECT_TRUE(FI.fileClose());
+	EXPECT_TRUE(fileInterface.fileClose());
 }
