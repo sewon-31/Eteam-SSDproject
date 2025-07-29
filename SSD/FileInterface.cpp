fcb27049e28ca0bb5859b9a7ddd06efad42c20fa
ies.cho <ies.cho@sec.com>
2025-07-29 14:41:34 +0900

    Init

---
+++ SSD/FileInterface.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+class FileInterface {
+public:
+	bool fileOpen(std::string fileName) {
+
+	};
+	std::string fileRead(std::string fileName) {
+
+	};
+	bool fileWrite(std::string fileName, std::string str) {
+
+	};
+};
---
+++ SSD/FileName_Test.cpp
@@ -0,0 +1,8 @@
+#include "gmock/gmock.h"
+
+using namespace testing;
+
+TEST(TC, TC_BASE) {
+
+	EXPECT_EQ(1,1);
+}
