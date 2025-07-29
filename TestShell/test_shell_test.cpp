5b1dfe7d6b0713b2ec42f7267913a8679909f9d4
Jaeyeong Jeon <zaeng616@gmail.com>
2025-07-29 15:49:16 +0900

    [feature] add invalid SSD case

--- TestShell/test_shell_test.cpp
+++ TestShell/test_shell_test.cpp
@@ -17,12 +17,18 @@ TEST_F(WriteTestFixture, TestBasicWrite) {
 TEST_F(WriteTestFixture, TestWriteInvalidLBAOverUpperBound) {
 	TestShell shell{ &ssd };
 	std::string value = "0x12345678";
-	EXPECT_CALL(ssd, write(OVER_LBA, value)).Times(0);
+	EXPECT_CALL(ssd, write).Times(0);
 	shell.write(OVER_LBA, value);
 }
 
 TEST_F(WriteTestFixture, TestWriteInvalidLBAUnderLowerBound) {
 	TestShell shell{ &ssd };
-	EXPECT_CALL(ssd, write(UNDER_LBA, value)).Times(0);
+	EXPECT_CALL(ssd, write).Times(0);
 	shell.write(UNDER_LBA, value);
+}
+
+TEST_F(WriteTestFixture, TestInvalidSSD) {
+	TestShell shell;
+	shell.write(VALID_LBA, value);
+	EXPECT_CALL(ssd, write).Times(0);
 }
