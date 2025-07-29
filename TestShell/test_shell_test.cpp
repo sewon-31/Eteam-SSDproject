b760313b7380713c0a55a4783cae5b4694ee0f09
Jaeyeong Jeon <zaeng616@gmail.com>
2025-07-29 15:11:41 +0900

    [feature] add basic write TC & feature

---
+++ TestShell/test_shell_test.cpp
@@ -0,0 +1,10 @@
+#include "gmock/gmock.h"
+#include "test_shell.h"
+TEST(ShellTest, TestBasicWrite) {
+	MockSSD ssd;
+	TestShell shell{ &ssd };
+	int lba = 10;
+	std::string value = "0x12345678";
+	EXPECT_CALL(ssd, write(lba, value)).Times(1);
+	shell.write(lba, value);
+}
