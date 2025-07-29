9f4f5f949a82d311c7a8cc06152e2ae77228f883
Kim Dokyoung <doky0130@gmail.com>
2025-07-29 14:46:39 +0900

    [feature] apply if _DEBUG test / else run

--- TestShell/main.cpp
+++ TestShell/main.cpp
@@ -2,7 +2,21 @@
 
 using namespace testing;
 
+#if _DEBUG
+
 int main() {
 	InitGoogleMock();
 	return RUN_ALL_TESTS();
 }
+
+#else
+
+int main() {
+	TestShell shell;
+
+	shell.runShell();
+	
+	return 0;
+}
+
+#endif
