bef6e8591c7ae15c0dbcb60ee7399e52bb520a19
Jaeyeong Jeon <zaeng616@gmail.com>
2025-07-29 15:25:06 +0900

    [feature] add invalid LBA case

--- TestShell/test_shell_test.cpp
+++ TestShell/test_shell_test.cpp
@@ -8,3 +8,12 @@ TEST(ShellTest, TestBasicWrite) {
 	EXPECT_CALL(ssd, write(lba, value)).Times(1);
 	shell.write(lba, value);
 }
+
+TEST(ShellTest, TestWriteInvalidLBA) {
+	MockSSD ssd;
+	TestShell shell{ &ssd };
+	int lba = 100;
+	std::string value = "0x12345678";
+	EXPECT_CALL(ssd, write(lba, value)).Times(0);
+	shell.write(lba, value);
+}
