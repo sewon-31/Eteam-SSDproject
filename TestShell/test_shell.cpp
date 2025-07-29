29cc766e75e18f66d05b3bbe39e4a2bc9d83401c
Jaeyeong Jeon <zaeng616@gmail.com>
2025-07-29 15:11:41 +0900

    [feature] add basic write TC & feature

--- TestShell/test_shell.cpp
+++ TestShell/test_shell.cpp
@@ -1,7 +1,12 @@
 #include "test_shell.h"
+<<<<<<< HEAD
 
 using std::cout;
 
 void TestShell::read(int lba) {
 	cout << ssd->read(lba);
+}
+
+void TestShell::write(int lba, std::string value) {
+	ssd->write(lba, value);
 }
