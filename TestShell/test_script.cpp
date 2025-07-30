14bd24b520067703d9792d9a714b98a9e43ba708
dooyeun <ghkdendus330@gmail.com>
2025-07-30 14:33:32 +0900

    [refactoring] test scripts command로 실행할 수 있도록 refactoring

---
+++ TestShell/test_script.cpp
@@ -0,0 +1,50 @@
+#include <random>
+
+#include "test_script.h"
+
+int ScriptsCommand::generateRandomIntValue() const
+{
+	static std::mt19937 rng{ std::random_device{}() };
+	static std::uniform_int_distribution<uint32_t> dist(0, 0xFFFFFFFF);
+
+	return dist(rng);
+}
+
+std::string ScriptsCommand::intToHexString(int hexValue) const
+{
+#if _DEBUG
+	return "0x00012345";
+#else
+	char buffer[11];
+	sprintf_s(buffer, "0x%08X", hexValue);
+	return std::string(buffer);
+#endif
+}
+
+bool ScriptsCommand::readCompare(int address, std::string hexValue) const
+{
+	if (ssd->read(address) == hexValue) return true;
+	return false;
+}
+
+void ScriptsFullWriteAndReadCompare::run(void) {
+	for (int startLba = 0; startLba < MAX_LBA; startLba += OPERATE_COUNT_PER_LOOP) {
+		string data = intToHexString(generateRandomIntValue());
+		// Write
+		for (int offset = 0; offset < OPERATE_COUNT_PER_LOOP; offset++) {
+			int lba = startLba + offset;
+			ssd->write(lba, data);
+		}
+
+		// Read & Compare
+		for (int offset = 0; offset < OPERATE_COUNT_PER_LOOP; offset++) {
+			int lba = startLba + offset;
+
+			if (readCompare(lba, data)) {
+				std::cout << "FAIL";
+				return;
+			}
+		}
+	}
+	std::cout << "PASS";
+}
---
+++ TestShell/test_script.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+
+#include "ssd_interface.h"
+
+class ScriptsCommand {
+public:
+	ScriptsCommand(SSDInterface* device) : ssd(device) {}
+	virtual void run() = 0;
+
+protected:
+	int generateRandomIntValue() const;
+	std::string intToHexString(int hexValue) const;
+	bool readCompare(int address, std::string hexValue) const;
+
+	SSDInterface* ssd;
+};
+
+
+class ScriptsFullWriteAndReadCompare : public ScriptsCommand {
+public:
+	ScriptsFullWriteAndReadCompare(SSDInterface* device) : ScriptsCommand(device) {}
+	void run() override;
+	
+private:
+	const static int MAX_LBA = 100;
+	const static int OPERATE_COUNT_PER_LOOP = 5;
+	const static int LOOP_COUNT = MAX_LBA / OPERATE_COUNT_PER_LOOP;
+};
