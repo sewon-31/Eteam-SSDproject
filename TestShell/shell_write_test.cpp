7d0da1c20f2effa4c623eb7837d01625f26decca
Jaeyeong Jeon <zaeng616@gmail.com>
2025-07-30 13:41:15 +0900

    [refactoring] refactoring for Test Shell Write TC

--- TestShell/shell_write_test.cpp
+++ TestShell/shell_write_test.cpp
@@ -8,16 +8,31 @@ public:
 	MockSSD mockSSD;
 	SSDDriver realSSD;
 	std::string value = "0x12345678";
+	std::ostringstream oss;
+	std::streambuf* oldCoutStreamBuf;
 	const std::string SSD_EXE_FILE = "./SSD.exe";
+	const std::string WRITE_DONE = "[WRITE] Done\n";
+	const std::string WRITE_FAIL = "[WRITE] Fail\n";
+	const std::string FULL_WRITE_DONE = "[FULL_WRITE] Done\n";
+	const std::string FULL_WRITE_FAIL = "[FULL_WRITE] Fail\n";
 	bool isFileExists(const std::string& path) {
 		std::ifstream file(path);
 		return file.good();
+	}
+protected:
+	void SetUp() override {
+		oldCoutStreamBuf = std::cout.rdbuf();
+		std::cout.rdbuf(oss.rdbuf());
+	}
+	void TearDown() override {
+		std::cout.rdbuf(oldCoutStreamBuf);
 	}
 };
 TEST_F(WriteTestFixture, TestBasicWrite) {
 	TestShell shell{ &mockSSD };
 	EXPECT_CALL(mockSSD, write(VALID_LBA, value)).Times(1);
 	shell.write(VALID_LBA, value);
+	EXPECT_EQ(WRITE_DONE, oss.str());
 }
 
 TEST_F(WriteTestFixture, TestWriteInvalidLBAOverUpperBound) {
@@ -44,18 +59,14 @@ TEST_F(WriteTestFixture, TestFullWrite) {
 	for (int i = 0; i < 100; i++)
 		EXPECT_CALL(mockSSD, write(i, value)).Times(1);
 	shell.fullWrite(value);
+	EXPECT_EQ(FULL_WRITE_DONE, oss.str());
 }
 
 TEST_F(WriteTestFixture, TestRealSSDWriteFail) {
 	if (isFileExists(SSD_EXE_FILE)) {
 		GTEST_SKIP() << SSD_EXE_FILE << " not found, skipping test.";
 	}
-	std::ostringstream oss;
-	auto oldCoutStreamBuf = std::cout.rdbuf();
-	std::cout.rdbuf(oss.rdbuf());
 	TestShell shell{ &realSSD };
 	shell.write(VALID_LBA, value);
-	std::cout.rdbuf(oldCoutStreamBuf);
-	string expect = "[WRITE] Fail\n";
-	EXPECT_EQ(expect, oss.str());
+	EXPECT_EQ(WRITE_FAIL, oss.str());
 }
