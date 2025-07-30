3cfeda61d457bf5b38b7f0c2c1b96004c554afce
dooyeun <ghkdendus330@gmail.com>
2025-07-30 14:33:32 +0900

    [refactoring] test scripts command로 실행할 수 있도록 refactoring

---
+++ TestShell/test_scripts_test.cpp
@@ -0,0 +1,50 @@
+#include "gmock/gmock.h"
+#include "test_script.h"
+#include <string>
+
+using namespace testing;
+using namespace std;
+
+class TestScriptsFixture : public Test {
+public:
+	void SetUp() override {
+		script = new ScriptsFullWriteAndReadCompare(mockSSD);
+	}
+	NiceMock<MockSSD> *mockSSD;
+	string invalidData = "0xFFFFFFFF";
+	string testData = "0x00012345";
+
+	ScriptsFullWriteAndReadCompare* script;
+	ostringstream oss;
+};
+
+TEST_F(TestScriptsFixture, Success) {
+	auto oldCoutStreamBuf = std::cout.rdbuf();
+	std::cout.rdbuf(oss.rdbuf());
+
+	EXPECT_CALL((*mockSSD), write(_, _))
+		.Times(100);
+	EXPECT_CALL((*mockSSD), read(_))
+		.Times(100)
+		.WillOnce(Return(testData));
+	
+	script->run();
+
+	std::cout.rdbuf(oldCoutStreamBuf);
+	EXPECT_EQ("PASS", oss.str());
+}
+
+TEST_F(TestScriptsFixture, FailWithInvalidData) {
+	auto oldCoutStreamBuf = std::cout.rdbuf();
+	std::cout.rdbuf(oss.rdbuf());
+
+	EXPECT_CALL((*mockSSD), read(_))
+		.WillOnce(Return(invalidData));
+	EXPECT_CALL((*mockSSD), read(_))
+		.WillOnce(Return(invalidData));
+
+	script->run();
+
+	std::cout.rdbuf(oldCoutStreamBuf);
+	EXPECT_EQ("FAIL", oss.str());
+}
