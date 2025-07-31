63492ed908d6f73398962b9333d247a00743a812
Jaeyeong Jeon <zaeng616@gmail.com>
2025-07-31 13:58:43 +0900

    [feature] write to file(../log/latest.log)

--- TestShell/logger.h
+++ TestShell/logger.h
@@ -6,7 +6,10 @@ public:
     void log(const char* funcName, const char* fmt, ...);
 
 private:
+    const std::string logDir = "../log";
+    const std::string logFile = logDir + "/latest.log";
     Logger() = default;
     std::string getCurrentTimestamp();
     std::string getLogMessage(const char* funcName, const char* fmt, va_list args);
+    void writeToFile(const std::string& log_msg);
 };
