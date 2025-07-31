bc3dfcba71ef2a8eb35ae0ad9b024bca9be23598
dooyeun <ghkdendus330@gmail.com>
2025-07-31 15:40:26 +0900

    [refactoring] flush command를 command pattern으로 구현하기 위해 리팩토링

--- TestShell/command.cpp
+++ TestShell/command.cpp
@@ -75,6 +75,7 @@ bool FullWriteCommand::execute(const std::vector<std::string>& args)
 	fullWrite(value);
     return true;
 }
+
 void FullWriteCommand::fullWrite(std::string value) {
 	if (ssd == nullptr) return;
 	try {
@@ -132,7 +133,7 @@ bool EraseCommand::execute(const std::vector<std::string>& args)
 	int size = std::stoi(args.at(2));
 	std::cout << "Executing erase" << std::endl;
 	erase(lba, size);
-	return false;
+	return true;
 }
 
 void EraseCommand::erase(int lba, int size) {
@@ -157,5 +158,44 @@ void EraseCommand::erase(int lba, int size) {
 	}
 	catch (SSDExecutionException& e) {
 		std::cout << "[ERASE] Fail" << std::endl;
+	}
+}
+
+bool EraseRangeCommand::execute(const std::vector<std::string>& args)
+{
+	int startLba = std::stoi(args.at(1));
+	int endLba = std::stoi(args.at(2));
+	std::cout << "Executing erase_range" << std::endl;
+	eraseRange(startLba, endLba);
+	return true;
+}
+
+void EraseRangeCommand::eraseRange(int startLba, int endLba) {
+	if (startLba > endLba)
+		std::swap(startLba, endLba);
+	try {
+		erase(startLba, endLba - startLba + 1);
+		std::cout << "[ERASE RANGE] Done" << std::endl;
+	}
+	catch (SSDExecutionException& e) {
+		std::cout << "[ERASE RANGE] Fail" << std::endl;
+	}
+}
+
+bool FlushCommand::execute(const std::vector<std::string>& args)
+{
+	std::cout << "Executing flush" << std::endl;
+	flush();
+	return true;
+}
+
+void FlushCommand::flush() {
+	try {
+		ssd->flush();
+		std::cout << "[FLUSH] Done" << std::endl;
+
+	}
+	catch (SSDExecutionException& e) {
+		std::cout << "[FLUSH] Fail" << std::endl;
 	}
 }
