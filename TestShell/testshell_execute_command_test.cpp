58038ee8b28bf3f02a06af612dad1d9ccdf05829
naing.yoon <naing.yoon@samsung.com>
2025-07-31 16:43:54 +0900

    [Refactoring] apply command factory to testcase

--- TestShell/testshell_execute_command_test.cpp
+++ TestShell/testshell_execute_command_test.cpp
@@ -1,7 +1,6 @@
-
+#pragma once
 #include "gmock/gmock.h"
-#include "test_shell.h"
-
+#include "mock_ssd.h"
 #include <string>
 
 using namespace testing;
@@ -9,7 +8,7 @@ using namespace std;
 
 //class MockTestShell : public TestShell {
 //public:
-//	MOCK_METHOD(void, read, (int lba));
+//	MOCK_METHOD(void, help, (int lba));
 //	MOCK_METHOD(void, write, (int lba, std::string value));
 //	MOCK_METHOD(void, fullRead, ());
 //	MOCK_METHOD(void, fullWrite, (std::string value));
@@ -17,44 +16,49 @@ using namespace std;
 //private:
 //};
 //
-//class TestShellCommandOperatorFixture : public Test {
-//public:
-//	void SetUp() override {
-//	}
-//	MockTestShell app;
-//};
-//TEST_F(TestShellCommandOperatorFixture, Read) {
-//	vector<string> commandVector = { "read", "3" };
-//
-//	EXPECT_CALL(app, read(_))
-//		.Times(1);
-//
-//	app.ExecuteCommand(commandVector);
-//}
-//TEST_F(TestShellCommandOperatorFixture, Write) {
-//	vector<string> commandVector = { "write", "3", "0xAAAAAAAA"};
-//
-//	EXPECT_CALL(app, write(_, _))
-//		.Times(1);
-//
-//	app.ExecuteCommand(commandVector);
-//}
-//TEST_F(TestShellCommandOperatorFixture, FullRead) {
-//	vector<string> commandVector = { "fullread" };
-//
-//	EXPECT_CALL(app, fullRead)
-//		.Times(1);
-//
-//	app.ExecuteCommand(commandVector);
-//}
-//TEST_F(TestShellCommandOperatorFixture, FullWrite) {
-//	vector<string> commandVector = { "fullwrite", "0xAAAAAAAA"};
-//
-//	EXPECT_CALL(app, fullWrite(_))
-//		.Times(1);
-//
-//	app.ExecuteCommand(commandVector);
-//}
+class TestShellCommandOperatorFixture : public Test {
+public:
+	TestShell app;
+
+	void SetUp() override {
+		app.setSSD(&mockSSD);
+	}
+public:
+	MockSSD mockSSD;
+};
+
+TEST_F(TestShellCommandOperatorFixture, Read) {
+	vector<string> commandVector = { "read", "3" };
+
+	EXPECT_CALL(mockSSD, read(_))
+		.Times(1);
+
+	app.ExecuteCommand(commandVector);
+}
+TEST_F(TestShellCommandOperatorFixture, Write) {
+	vector<string> commandVector = { "write", "3", "0xAAAAAAAA"};
+
+	EXPECT_CALL(mockSSD, write(_, _))
+		.Times(1);
+
+	app.ExecuteCommand(commandVector);
+}
+TEST_F(TestShellCommandOperatorFixture, FullRead) {
+	vector<string> commandVector = { "fullread" };
+
+	EXPECT_CALL(mockSSD, read(_))
+		.Times(100);
+
+	app.ExecuteCommand(commandVector);
+}
+TEST_F(TestShellCommandOperatorFixture, FullWrite) {
+	vector<string> commandVector = { "fullwrite", "0xAAAAAAAA"};
+
+	EXPECT_CALL(mockSSD, write(_, _))
+		.Times(100);
+
+	app.ExecuteCommand(commandVector);
+}
 //TEST_F(TestShellCommandOperatorFixture, Help) {
 //	vector<string> commandVector = { "help" };
 //
