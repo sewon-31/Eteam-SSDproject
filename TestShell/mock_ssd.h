735545483eac51cc4434b4a29f24b06c672c5fff
dooyeun <ghkdendus330@gmail.com>
2025-07-31 10:59:00 +0900

    [feature] TestShell: erase ssddriver에 추가하기

--- TestShell/mock_ssd.h
+++ TestShell/mock_ssd.h
@@ -5,6 +5,7 @@ class MockSSD : public SSDInterface {
 public:
 	MOCK_METHOD(void, write, (int lba, string value), (override));
 	MOCK_METHOD(string, read, (int lba), (override));
+	MOCK_METHOD(void, erase, (int lba, int size), (override));
 };
 
 class MockSSDDriver : public SSDDriver {
--- TestShell/ssd_interface.cpp
+++ TestShell/ssd_interface.cpp
@@ -18,3 +18,11 @@ string SSDDriver::read(int lba) {
 
 	return content;
 }
+
+void SSDDriver::erase(int lba, int size)
+{
+	string command = "\"ssd E " + std::to_string(lba) + std::to_string(size) + " >nul 2>&1\"";
+	if (runExe(command) == false) {
+		throw SSDExecutionException("Execution failed: " + command);
+	}
+}
