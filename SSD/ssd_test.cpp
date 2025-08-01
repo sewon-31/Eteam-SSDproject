357351a11534f881717f4103641c0987964e460c
sewon-31 <oneoctober12@gmail.com>
2025-08-01 15:24:13 +0900

    [Refactoring] refactor unit tests

--- SSD/ssd_test.cpp
+++ SSD/ssd_test.cpp
@@ -186,12 +186,13 @@ TEST_F(SSDTestFixture, TC_FULL_WRITE_READ) {
 	app.run({ "R", "0" });
 
 	for (int i = 0; i < 100; i++) {
-		if (app.getData(i) != str[i]) {
-			ret = false;
-			break;
-		}
+		EXPECT_EQ(app.getData(i), str[i]);
+		//if (app.getData(i) != str[i]) {
+		//	ret = false;
+		//	break;
+		//}
 	}
-	EXPECT_TRUE(ret);
+	//EXPECT_TRUE(ret);
 }
 
 TEST_F(SSDTestFixture, TC_WRITE_OUTPUT) {
@@ -202,40 +203,7 @@ TEST_F(SSDTestFixture, TC_WRITE_OUTPUT) {
 	EXPECT_EQ(12, outputFile.checkSize());
 }
 
-TEST_F(SSDTestFixture, TC_RUN_WRITE) {
-	app.run({ "W", "0", "0x11112222" });
-
-	//FileInterface nandFile = { "../ssd_nand.txt" };
-
-	string actual;
-	nandFile.fileOpen();
-	nandFile.fileReadOneline(actual);
-	nandFile.fileClose();
-
-	EXPECT_EQ(actual, "0x11112222");
-}
-
-TEST_F(SSDTestFixture, TC_RUN_READ) {
-	app.run({ "R", "0" });
-
-	//FileInterface nandFile = { "../ssd_nand.txt" };
-
-	string expected;
-	nandFile.fileOpen();
-	nandFile.fileReadOneline(expected);
-	nandFile.fileClose();
-
-	//FileInterface outputFile = { "../ssd_output.txt" };
-
-	string actual;
-	outputFile.fileOpen();
-	outputFile.fileReadOneline(actual);
-	outputFile.fileClose();
-
-	EXPECT_EQ(expected, actual);
-}
-
-TEST_F(SSDTestFixture, TC_RUN_ERASE) {
+TEST_F(SSDTestFixture, TC_ERASE) {
 	string str[100];
 	char buffer[16];
 	char ret = true;
@@ -248,11 +216,9 @@ TEST_F(SSDTestFixture, TC_RUN_ERASE) {
 
 	EXPECT_EQ(1200, nandFile.checkSize());
 
-	// to update storage from nand file
 	app.run({ "E", "0", "10" });
 
 	app.run({ "R", "0" });
 	EXPECT_EQ(app.getData(9), "0x00000000");
 	EXPECT_EQ(app.getData(10), str[10]);
 }
-
