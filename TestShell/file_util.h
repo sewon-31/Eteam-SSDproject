89d626bbd9cd3517be8ede186142dd54a179b62c
Seungah Lim <sss.lim@samsung.com>
2025-08-01 11:03:12 +0900

    [refactoring] use filesystem as C++17 can be available

--- TestShell/file_util.h
+++ TestShell/file_util.h
@@ -5,13 +5,16 @@
 
 class FileUtil {
 public:
-    static bool directoryExists(const std::string& path);
-    static bool createDirectory(const std::string& path);
-    static bool fileExists(const std::string& path);
-    static bool writeLine(const std::string& filePath, const std::string& line, bool append = true);
-    static bool readLine(const std::string& filePath, std::string& lineOut);
-    static bool clearFile(const std::string& filePath);
-    static bool writeAllLines(const std::string& filePath, const std::vector<std::string>& lines, bool append = false);
-    static bool readAllLines(const std::string& filePath, std::vector<std::string>& linesOut);
-    static size_t getFileSize(const std::string& filePath);
+	static bool directoryExists(const std::string& path);
+	static bool createDirectory(const std::string& path);
+	static bool fileExists(const std::string& path);
+
+	static bool writeLine(const std::string& filePath, const std::string& line, bool append = true);
+	static bool readLine(const std::string& filePath, std::string& lineOut);
+	static bool clearFile(const std::string& filePath);
+
+	static bool writeAllLines(const std::string& filePath, const std::vector<std::string>& lines, bool append = false);
+	static bool readAllLines(const std::string& filePath, std::vector<std::string>& linesOut);
+
+	static size_t getFileSize(const std::string& filePath);
 };
